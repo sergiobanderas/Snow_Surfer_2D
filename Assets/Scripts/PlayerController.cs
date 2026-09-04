ccdf7f99f50831a7da8abcd11d94816481e4225d
sergiobanderas <sergio.banderas@fpalanturing.es>
2026-09-04 18:58:24 +0200

    BootsPlayer when pressing Up

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,33 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float torqueForce = 2.5f;
+    [SerializeField] private float baseSpeed = 15f;
+    [SerializeField] private float boostMultiplier = 20f;
 
     private Rigidbody2D rb;
+    SurfaceEffector2D surfaceEffector2D;
 
     private InputAction moveAction;
+
+    Vector2 moveValue;
     
     void Start()
     {        
         moveAction = InputSystem.actions.FindAction("Move");
         rb = GetComponent<Rigidbody2D>();   
+        surfaceEffector2D = FindAnyObjectByType<SurfaceEffector2D>();
     }
 
     
     void Update()
     {
-        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        RotatePlayer();
+        BoostPlayer();
+    }
+
+    void RotatePlayer()
+    {
+        moveValue = moveAction.ReadValue<Vector2>();
         if (moveValue.x < 0)
         {
             rb.AddTorque(torqueForce);
@@ -27,5 +39,20 @@ public class PlayerController : MonoBehaviour
         {
             rb.AddTorque(-torqueForce);
         }
+    }
+
+    void BoostPlayer()
+    {
+        
+         if (moveValue.y > 0)
+        {
+            Debug.Log("Boosting");
+            surfaceEffector2D.speed = boostMultiplier;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+
     }
 }
